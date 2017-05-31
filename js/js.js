0d0fe6df0eb5e74abb2f57fc76d815b61dfb68b9
LuisRigo <luis.rigo.vega@gmail.com>
2017-05-31 18:19:54 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,44 @@
+﻿var countjson = 0;
+var arrayjson = ["json1.json", "json2.json"];
+$(function() {
+	
+  
+    $( "#sj" ).click(function() { cargarNoticia();});
+  
+  
+});
+
+
+function cargarNoticia() {
+    if (countjson < arrayjson.length) {
+        var name = arrayjson[countjson];
+        countjson++;
+        $.getJSON("https://rawgit.com/LuisRigo/pruebas-noticias/master/" + name, function (jsonObject) {
+            addnoticias(jsonObject);
+        });
+    } else {
+        alert("No hay mas noticias");
+    }
+}
+function addnoticias(json){
+	$.each( json, function( i, noticia ) {
+         $("#masnoticias").append(
+		 '<div id="'+noticia.id+'" class="container-fluid well section">'+
+			"<h2>"+noticia.titulo+"</h2>"+
+			"<p>"+noticia.descripcion+"</p>"+
+			"<img class='img-responsive' src="+'"'+noticia.img+'"'+"alt='huerfano'>"+"<br>"+
+			"<span class='glyphicon glyphicon-calendar'>"+"</span>"+"<span>"+noticia.datetime+"</span>"+
+		 "</div>"
+		 );
+		 $("#navebar").append(
+		 "<li>"+'<a href="#'+noticia.id+'">'+noticia.nav_element+"</a>"+"</li>"
+		 );
+		
+     }); 
+}
+$(window).scroll(function(){
+     if ($(window).scrollTop() == $(document).height() - $(window).height()){
+      cargarNoticia();
+     }					
+    });
+
